11ff988ef3d0a762001c03a7747066f2d543864a
Nicholas A. Del Grosso <delgrosso@bio.lmu.de>
2017-05-12 16:36:14 +0200

    Added texture and flat shading

--- experiments/check_alignment.py
+++ experiments/check_alignment.py
@@ -3,4 +3,6 @@ from app import RatcaveApp
 import cfg
 
 app = RatcaveApp(arena_objfile=cfg.ARENA_FILENAME, projector_file=cfg.PROJECTOR_FILENAME)
+app.arena.texture = cfg.ARENA_LIGHTING_TEXTURE
+app.arena.uniforms['flat_shading'] = cfg.ARENA_LIGHTING_FLAT_SHADING
 app.run()
