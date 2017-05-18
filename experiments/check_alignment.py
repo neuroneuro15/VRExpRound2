94e0d65e4411d71bd6a4541f91d5a67ffecafc5c
Nicholas A. Del Grosso <delgrosso@bio.lmu.de>
2017-05-18 14:59:22 +0200

    Added lighting control, to make sure light levels are the same across sessions.

--- experiments/check_alignment.py
+++ experiments/check_alignment.py
@@ -2,6 +2,13 @@ from __future__ import print_function
 from app import RatcaveApp
 import cfg
 
+from pypixxlib import propixx
+
+projector = propixx.PROPixx()
+projector.setSleepMode(not cfg.PROJECTOR_TURNED_ON)
+projector.setLampLED(cfg.PROJECTOR_LED_ON)
+projector.setLedIntensity("6.25")
+
 app = RatcaveApp(arena_objfile=cfg.ARENA_FILENAME, projector_file=cfg.PROJECTOR_FILENAME)
 app.arena.texture = cfg.ARENA_LIGHTING_TEXTURE
 app.arena.uniforms['flat_shading'] = cfg.ARENA_LIGHTING_FLAT_SHADING
