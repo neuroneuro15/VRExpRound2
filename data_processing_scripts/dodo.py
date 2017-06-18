02f7d822082f3a6043d2b1407d11d3580f2a1d7d
Nicholas A. Del Grosso <delgrosso@bio.lmu.de>
2017-06-18 21:38:56 +0200

    added junk info at end of filename in cases where duplicated filenames were made (hand-typed filenames)

--- data_processing_scripts/dodo.py
+++ data_processing_scripts/dodo.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 from os import path
-
+from datetime import datetime
 
 basedir = r"Z://nickdg/data/VR_Experiments_Round_2"
 take_basedir = path.join(basedir, 'Motive Files', 'VR_Experiment_Data', 'MotiveProjects_2')
@@ -9,17 +9,21 @@ converted_basedir = path.join(basedir, 'Converted Motive Files')
 
 def task_convert_take_files():
     """Turns the proprietary Motive Take file formatted-files from the experiment into CSV and AVI files."""
-    for currpath, pathnames, fnames in os.walk(take_basedir):
+    for idx1, (currpath, pathnames, fnames) in enumerate(os.walk(take_basedir)):
         take_files = [path.basename(fname) for fname in glob(path.join(currpath, '*.tak'))]
-        for take_file in take_files:
+        for idx2, take_file in enumerate(take_files):
             basename = path.splitext(take_file)[0]
+            if 'Exp' not in basename:
+                creation_datetime = datetime.fromtimestamp(path.getctime(path.join(currpath, take_file)))
+                basename = basename + '_' + creation_datetime.strftime('%Y%m%d_%H%M%S') + '_' + str(idx1) + '_' + str(idx2)
+            # print(path.join(currpath, basename))
             yield {
                 'file_dep': [path.join(currpath, take_file)],
                 'targets': [path.join(converted_basedir, basename, basename + '.csv')],
                 'actions': ['ipy64 take_to_csv.py "%(dependencies)s" "%(targets)s"'],
                 'name': 'to_csv: {}'.format(basename),
             }
-            print(currpath)
+
             yield {
                 'file_dep': [path.join(currpath, take_file)],
                 'targets': [path.join(converted_basedir, basename, basename + '.avi')],
